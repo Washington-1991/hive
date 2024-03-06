1ba25bf6aa9b642399ebb6f54986db2064e03aab
Washington Arambillete <milanghdarkoh@hotmail.com>
2024-03-06 14:22:14 +0100

    weight

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_06_124831) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_06_132114) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_06_124831) do
     t.index ["program_id"], name: "index_programs_workouts_on_program_id"
   end
 
+  create_table "user_weights", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.float "weight"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_weights_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -46,4 +54,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_06_124831) do
 
   add_foreign_key "programs", "users"
   add_foreign_key "programs_workouts", "programs"
+  add_foreign_key "user_weights", "users"
 end
