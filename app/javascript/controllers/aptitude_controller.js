998b94dd1b73055dfa4341ea50d72a18d1639a56
a.m. <arthurmaddi59270@gmail.com>
2024-03-08 11:39:07 +0100

    aptitude done

--- app/javascript/controllers/aptitude_controller.js
+++ app/javascript/controllers/aptitude_controller.js
@@ -2,9 +2,11 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="aptitude"
 export default class extends Controller {
-  static targets = ["input", "aptitudeTest", "image"];
+  static targets = ["input", "aptitudeTest", "image", "difficulty"];
 
-  connect() {}
+  connect() {
+    // console.log(this.testTarget.dataset.difficulty);
+  }
 
   nextTest(event) {
     const aptitudeTests = this.aptitudeTestTargets;
@@ -14,13 +16,23 @@ export default class extends Controller {
     if (currentTest.id === "carouselExampleRide3") {
       const form = this.element;
       const inputs = this.inputTargets;
-      const images = this.imageTargets.select((image) =>
+      const images = this.imageTargets;
+      const difficulties = this.difficultyTargets;
+
+      const activeImages = images.filter((image) =>
         image.classList.contains("active")
       );
 
-      inputs[0].value = images[0].dataset.difficulty;
-      inputs[1].value = images[1].dataset.difficulty;
-      inputs[2].value = images[2].dataset.difficulty;
+      // console.log(activeImages);
+      // console.log(activeImages[0]);
+      // console.log(activeImages[0].dataset);
+      // console.log(activeImages[0].dataset.difficulty);
+
+      // const filteredElements = Array.from(nodeList).filter(condition);
+
+      inputs[0].value = activeImages[0].dataset.difficulty;
+      inputs[1].value = activeImages[1].dataset.difficulty;
+      inputs[2].value = activeImages[2].dataset.difficulty;
 
       form.submit();
     } else {
