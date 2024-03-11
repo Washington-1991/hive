5d4541e4037174f3f3343bfd00c3da79d04e2d14
a.m. <arthurmaddi59270@gmail.com>
2024-03-11 10:02:41 +0100

    ad nickname and photo

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,8 +43,8 @@ User.destroy_all
 puts 'Creating user...'
 
 
-user = User.create!(email: "user@mail.com", password: "password")
-User.create!(email: "nico@mail.com", password:"coucou")
+user = User.create!(email: "user@mail.com", password: "password", nickname: "arthur", photo_url:("https://images.unsplash.com/photo-1557862921-37829c790f19?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"))
+User.create!(email: "nico@mail.com", password:"coucou", nickname: "nico", photo_url:("https://images.unsplash.com/photo-1568602471122-7832951cc4c5?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D") )
 
 
 #create program
