6d77d514e51ae5021fc6fef2963bc81990fa80c6
Nicolas Guillon <nicolas.guillon95@yahoo.com>
2024-03-12 17:44:24 +0100

    update programs and seed

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,4 +1,10 @@
 class ProgramsController < ApplicationController
   def index
+    @echauffement = Exercice.where(category:"Echauffement").all.sample
+    @bras = Exercice.where(category:"Bras").all.sample
+    @jambes = Exercice.where(category:"Jambes").all.sample
+    category = ["Pecs", "Dos"].sample
+    @other = Exercice.where(category: category).all.sample
+
   end
 end
