e1374df57db5e9a50337a08d9710ca13c983ee6a
Washington Arambillete <milanghdarkoh@hotmail.com>
2024-03-12 15:04:11 +0100

    20241503

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_11_083131) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_12_134805) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,7 +30,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_11_083131) do
     t.integer "time"
     t.integer "repetition"
     t.float "calories"
-    t.string "type"
+    t.string "category"
     t.string "description"
     t.bigint "workout_id", null: false
     t.datetime "created_at", null: false
